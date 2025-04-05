5489132a053c9901a1f0ce6e530f61db81efda6a
viashchuk <victoriya.dev@icloud.com>
2025-04-05 12:45:10 +0200

    Send first message

---
+++ task-3/build.gradle.kts
@@ -0,0 +1,24 @@
+val ktor_version: String by project
+val logback_version: String by project
+val typesafe_version: String by project
+
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.typesafe:config:$typesafe_version")
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ task-3/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import com.typesafe.config.ConfigFactory
+
+suspend fun main() {
+    val config = ConfigFactory.load()
+    val bot_token:String = config.getString("discord.botToken")
+    val chanel_id:String = config.getString("discord.chanelID")
+
+    val client = HttpClient(CIO)
+    val response: HttpResponse = client.post("https://discord.com/api/v10/channels/$chanel_id/messages") {
+        contentType(ContentType.Application.Json)
+        headers {
+            append(HttpHeaders.Authorization, "Bot $bot_token")
+        }
+        setBody("""{"content": "Hello, World!"}""")
+    }
+    println(response.status)
+    client.close()
+}
