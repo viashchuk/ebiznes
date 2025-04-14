6a53b2c22d2c9ea7c2287f92590a91e30d37b823
viashchuk <victoriya.dev@icloud.com>
2025-04-14 19:36:04 +0200

    Fix cart

--- task-4/models/cart.go
+++ task-4/models/cart.go
@@ -8,7 +8,7 @@ type Cart struct {
 	gorm.Model
 	Quantity   int      `json:"quantity"`
 	ProductID  int      `json:"product_id"`
-	Product    Product  `gorm:"foreignKey:ProductID" json:"product"`
+	Product    Product  `gorm:"foreignKey:ProductID" json:"-"`
 	CustomerID uint     `json:"customer_id"`
 	Customer   Customer `gorm:"foreignKey:CustomerID" json:"-"`
 }
