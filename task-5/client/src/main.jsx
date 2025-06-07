ed36ac9b39db47d7e7f4e8ef4849ba9d925a999a
viashchuk <victoriya.dev@icloud.com>
2025-06-07 18:13:50 +0200

    Fix CORS

--- task-5/client/src/main.jsx
+++ task-5/client/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
-
+https://ebiznes-backend-lab-c5dpe4aqaxbubxdt.polandcentral-01.azurewebsites.net/products
 
 if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://localhost:1323'
 }
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'ebiznes-backend-lab-c5dpe4aqaxbubxdt.polandcentral-01.azurewebsites.net'
+  axios.defaults.baseURL = 'https://ebiznes-backend-lab-c5dpe4aqaxbubxdt.polandcentral-01.azurewebsites.net'
 }
 
 createRoot(document.getElementById('root')).render(
--- task-5/server/server.go
+++ task-5/server/server.go
@@ -20,7 +20,7 @@ func main() {
 
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-        AllowOrigins: []string{"http://localhost:5173", "ebiznes-backend-lab-c5dpe4aqaxbubxdt.polandcentral-01.azurewebsites.net"},
+        AllowOrigins: []string{"http://localhost:5173", "https://ebiznes-backend-lab-c5dpe4aqaxbubxdt.polandcentral-01.azurewebsites.net"},
         AllowMethods: []string{echo.GET, echo.POST},
     }))
 
