f77f78cedca80cf45108be4cc7bb0dc0e598678e
viashchuk <victoriya.dev@icloud.com>
2025-06-07 18:00:55 +0200

    Update CORS

--- task-5/client/src/main.jsx
+++ task-5/client/src/main.jsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = 'http://localhost:1323'
+}
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'ebiznes-backend-lab-c5dpe4aqaxbubxdt.polandcentral-01.azurewebsites.net'
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
